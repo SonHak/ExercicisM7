be66acd39fde52ff597d0d613c439f060964bc0e
Adrian Cardenas <SonHak@github.com>
2017-09-25 17:24:51 +0200

    pujo un nou exercici

---
+++ m7/Server.php
@@ -0,0 +1,10 @@
+<?php
+
+	foreach( $_SERVER as $clau => $valor ){
+	
+		echo "la clau es " . $clau ." i té el valor " . $valor . "</br>";
+	}
+	
+	//2.- clau  HTTP_USER_AGENT
+	//3.- clau HTTP_ACCEPT_LANGUAGE
+?>
