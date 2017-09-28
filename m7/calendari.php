025f9315d45cad9b20e97c6be0d3e96d8592ec4d
Adrian Cardenas <SonHak@github.com>
2017-09-28 16:23:54 +0200

    calendario

---
+++ m7/calendari.php
@@ -0,0 +1,23 @@
+<html>
+	<style>
+		td{border: 1px solid black; width: 100px;height: 100px;}
+	</style>
+	<body>
+	<table>
+		<tr>
+			<th>Lun</th><th>Mar</th><th>Mie</th><th>Jue</th>
+			<th>Vie</th><th>Sab</th><th>Dom</th>
+		</tr>
+	<?php
+		$mesos = ("Dilluns","Dimarts","Dimecres","Dijous","Divendres","Dissabte","Diumenge")
+		
+		echo "<td>2</td>";
+			
+
+
+	?>
+
+	</table>
+	</body>
+</html>
+
--- m7/complicat.php
+++ m7/complicat.php
@@ -1,14 +1,19 @@
-<?php
-$jugadors_de_lacrosse = array( "Billy Bitter", "Chris Bocklet", "Jeremy Boltus" );
-$jugadors_de_pilota_basca = array( "Iñaqui" );
-$esports = array();
-$esports["Lacrosse"] = $jugadors_de_lacrosse;
-$esports["Pilota Basca"] = $jugadors_de_pilota_basca;
+<html>
+	<body>
+	<?php
+	$jugadors_de_lacrosse = array( "Billy Bitter", "Chris Bocklet", "Jeremy Boltus" );
+	$jugadors_de_pilota_basca = array( "Iñaqui" );
+	$esports = array();
+	$esports["Lacrosse"] = $jugadors_de_lacrosse;
+	$esports["Pilota Basca"] = $jugadors_de_pilota_basca;
 
-foreach( $esports as $esport => $jugadors ) {
-    echo "Els meu jugadors preferits de $esport són ";
-    foreach ($jugadors as $esport => $jugadors ){
-			echo $jugadors . " ";
-		}
-    echo "<br>";
-}
+	foreach( $esports as $esport => $jugadors ) {
+		echo "Els meu jugadors preferits de $esport són ";
+		foreach ($jugadors as $esport => $jugadors ){
+				echo $jugadors . " ";
+			}
+		echo "<br>";
+	}
+	?>
+	</body>
+</html>
