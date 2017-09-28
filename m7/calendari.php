e98e1149a652c1d7f64a2b7e998cfa7d89c93a84
Adrian Cardenas <SonHak@github.com>
2017-09-28 17:49:12 +0200

    calendari

--- m7/calendari.php
+++ m7/calendari.php
@@ -20,7 +20,7 @@ $diaSemana=date("w",mktime(0,0,0,$mes,1,$año));
 		<tr>
 			<?php
 				$ultima_celda=$diaSemana+$totalDiasMes;
-				for($i=1;$i <=$totalDiasMes;$i++){
+				for($i=1;$i <=$42;$i++){
 					if($i==$diaSemana)
 						{
 							$dia=1;
