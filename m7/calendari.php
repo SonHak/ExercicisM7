d611f582454f5c9ad2ba358c226585c8f0f4a71a
Adrian Cardenas <SonHak@github.com>
2017-09-28 16:48:13 +0200

    proba2

--- m7/calendari.php
+++ m7/calendari.php
@@ -10,16 +10,18 @@ $ultimoDiaMes=date("d",(mktime(0,0,0,$mes+1,1,$año)-1));
 		td{border: 1px solid black; width: 100px;height: 100px;}
 	</style>
 	<body>
+		<!--
 	<table>
 		<th><?php echo $mes. "  ".$año  ?></th>
 		<tr>
 			<th>Lun</th><th>Mar</th><th>Mie</th><th>Jue</th>
 			<th>Vie</th><th>Sab</th><th>Dom</th>
 		</tr>
+		-->
 	<?php
 		$meses = array(1=> "Enero","Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio",
 					   "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre");
-		echo $diaSemana . $ultimoDiaMes;
+		echo "ultimo día de la semana" . $diaSemana ."ultimo dia del mes" . $ultimoDiaMes;
 
 			
 
