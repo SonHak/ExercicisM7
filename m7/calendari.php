0f5fee436b4d23c48c0a67ea522be6ff7be72e80
Adrian Cardenas <SonHak@github.com>
2017-09-28 16:42:09 +0200

    proba

--- m7/calendari.php
+++ m7/calendari.php
@@ -1,9 +1,13 @@
+$dia = date("j");
+$mes = date("n");
+$año = date("Y");
 <html>
 	<style>
 		td{border: 1px solid black; width: 100px;height: 100px;}
 	</style>
 	<body>
 	<table>
+		<th><?php echo $mes. "  ".$año  ?></th>
 		<tr>
 			<th>Lun</th><th>Mar</th><th>Mie</th><th>Jue</th>
 			<th>Vie</th><th>Sab</th><th>Dom</th>
@@ -11,9 +15,8 @@
 	<?php
 		$meses = array(1=> "Enero","Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio",
 					   "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre");
-		$mes = date("n");
-		$dia = date("j");
-		$ano = date ("Y");
+		
+		
 		echo "<td>$mes . $dia . $ano </td>";
 			
 
